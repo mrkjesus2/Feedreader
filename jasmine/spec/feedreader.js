0668ad5f18482b1d3d7e0aaf33a45af4fd57a573
Mark <markweig@gmail.com>
2016-03-19 12:50:28 -0400

    Add Initial Entries test
    Test for the presence of articles after loadfeed has finished it's
    work.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -77,13 +77,22 @@ $(function() {
 
     /* Test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+            loadFeed(0, function() {
+                done()
+            });
+         });
 
+         it('should have entries', function(done) {
+            var feedEntries = $('.entry');
+            console.log(feedEntries);
+            expect(feedEntries.length).toBeGreaterThan(0);
+            done()
+         });
     });
 
     /* Test suite named "New Feed Selection" */
